88d6921094059d349e7f48aa29664a70bc75ec97
Junior 2 in 1 <gilroyjr01@gmail.com>
2025-04-02 12:34:59 -0400

    working on part 4, need to finish by tmr

---
+++ Classwork/Part4.2CW/src/Student.java
@@ -0,0 +1,72 @@
+import java.io.*;
+import java.util.*;
+
+public class Student {
+
+    private String name;
+    private float grade;
+
+
+    public Student(String name, float grade)
+    {
+        this.grade = grade;
+        this.name =name;
+    }
+
+    /**
+     * tostring for testing file read
+     */
+    public String toString()
+    {
+        return name;
+    }
+
+    public static void main(String[] args) throws IOException{
+
+
+        Scanner sc = new Scanner(System.in);
+        ArrayList<Student> classroom = new ArrayList<>();
+        System.out.println("Enter input file name: ");
+        String inFile = sc.nextLine();
+
+        System.out.println("Enter output file name: ");
+        String outFile = sc.nextLine();
+
+        File f = new File(inFile);
+        Scanner input = new Scanner(f);
+
+        while(input.hasNext())
+        {
+            
+            String first = input.next();
+            String last = input.next();
+            float grade = input.nextFloat();
+
+            classroom.add(new Student(first +" " + last, grade));
+        }
+
+        PrintWriter pow = new PrintWriter(outFile);
+
+        for(int i =0; i<classroom.size()-1; i++)
+        {
+            pow.print(classroom.get(i) + ", ");
+        }
+
+        pow.println(classroom.get(classroom.size()-1));
+
+        float avg = 0f;
+        for(Student c : classroom)
+        {
+            avg += c.grade;
+        }
+
+        avg /= classroom.size();
+
+        pow.printf("\n%-15s %10.1f", "Average", avg);
+
+
+        pow.close();
+        input.close();
+
+    }
+}
--- changelog.md
+++ changelog.md
@@ -2,5 +2,8 @@
 
 **Last Change Location: June 2 in 1**
 
+## April 2nd, 2025
+- working on part 4 assignments
+
 ## March 31st, 2025
 - First commit made from 2 in 1, and first push to github repo
